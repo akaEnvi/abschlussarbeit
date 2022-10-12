aff78828ac07670cb702d8bfad5cf5de6f55ab41
Your Name <you@example.com>
2022-10-12 16:51:52 +0200

    Question-List init

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,7 @@
 <template>
-  <nav><router-link to="/">Home</router-link> |</nav>
+  <nav>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/questionList">Questions</router-link> |
+  </nav>
   <router-view />
 </template>
--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,18 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Index from "../views/IndexView.vue";
+import questionList from "@/views/questionList.vue";
 
 const routes = [
   {
     path: "/",
     name: "Index",
     component: Index,
+  },
+
+  {
+    path: "/questionList",
+    name: "question",
+    component: questionList,
   },
 ];
 
