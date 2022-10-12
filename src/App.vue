2cb13f3bab393271e39437dfde4ffa66f7c6392d
Jipson Minibhavan <jipson.minibhavan@googlemail.com>
2022-10-12 16:45:01 +0200

    feat:Roti/CheckIn

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,8 @@
 <template>
-  <nav><router-link to="/">Home</router-link> |</nav>
+  <nav>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/CheckIn">CheckIn</router-link>|
+    <router-link to="/Roti">Roti</router-link>|
+  </nav>
   <router-view />
 </template>
--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,19 @@
 import { createRouter, createWebHistory } from "vue-router";
-import Index from "../views/IndexView.vue";
-
+import Index from "@/views/IndexView.vue";
+import RotiEvaluation from "@/views/RotiEvaluation.vue";
+import CheckIn from "@/views/CheckIn.vue";
 const routes = [
   {
     path: "/",
     name: "Index",
     component: Index,
   },
+  {
+    path: "/CheckIn",
+    name: "CheckIn",
+    component: CheckIn,
+  },
+  { path: "/Roti", name: "RotiEvalution", component: RotiEvaluation },
 ];
 
 const router = createRouter({
