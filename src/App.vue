bb42b6341f4709d6135c44ea0827e60d4bd959b4
dreadderino1987 <sascha_dietrich@outlook.com>
2022-10-13 16:08:42 +0200

    feat: bugfix

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,8 @@
     <router-link to="/">Index</router-link> |
 
     <router-link to="/questionList">Questions</router-link> |
-
     <router-link to="/CheckIn">CheckIn</router-link> |
+    <router-link to="/AttendeeList">AttendeeList</router-link> |
     <router-link to="/Roti">Roti</router-link> |
   </nav>
   <router-view />
