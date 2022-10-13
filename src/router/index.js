49a00f99a632fe77f4fc22be36fed439033fe534
akaEnvi <envimth@gmail.com>
2022-10-13 17:05:09 +0200

    fix: Sorting or components and connection with navbar

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,12 @@
 import { createRouter, createWebHistory } from "vue-router";
 
-import Index from "../views/IndexView.vue";
-import questionList from "@/views/questionList.vue";
-
-import RotiEvaluation from "@/views/RotiEvaluation.vue";
-import CheckIn from "@/views/CheckIn.vue";
+import Index from "@/views/IndexView.vue";
+import CheckInView from "@/views/CheckInView.vue";
+import CheckOutView from "@/views/CheckOutView.vue";
+import LiveSessionView from "@/views/LiveSessionView.vue";
+import LearningListView from "@/views/LearningListView.vue";
+import QuestionListView from "@/views/QuestionListView.vue";
+import HubView from "@/views/HubView.vue";
 
 const routes = [
   {
@@ -12,19 +14,36 @@ const routes = [
     name: "Index",
     component: Index,
   },
-
   {
-    path: "/questionList",
-    name: "question",
-    component: questionList,
-  },
-
-  {
-    path: "/CheckIn",
+    path: "/CheckInView",
     name: "CheckIn",
-    component: CheckIn,
+    component: CheckInView,
   },
-  { path: "/Roti", name: "RotiEvalution", component: RotiEvaluation },
+  {
+    path: "/CheckOutView",
+    name: "CheckOut",
+    component: CheckOutView,
+  },
+  {
+    path: "/LiveSessionView",
+    name: "LiveSession",
+    component: LiveSessionView,
+  },
+  {
+    path: "/LearningListView",
+    name: "LearningList",
+    component: LearningListView,
+  },
+  {
+    path: "/QuestionListView",
+    name: "QuestionList",
+    component: QuestionListView,
+  },
+  {
+    path: "/HubView",
+    name: "HubView",
+    component: HubView,
+  },
 ];
 
 const router = createRouter({
