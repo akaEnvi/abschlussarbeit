3eb2da3445acd1696549ecb52a0b41c8509672fd
dreadderino1987 <sascha_dietrich@outlook.com>
2022-10-13 09:52:42 +0200

    bugfix

---
+++ src/views/CheckIn.vue
@@ -0,0 +1,20 @@
+<template>
+  <table>
+    <thead>
+      <tr>
+        Name:<br />
+        <input type="text" name="name" />
+        <br />
+        Was lief gestern suboptimal?
+        <br />
+        <textarea placeholder=" Was lief gestern suboptimal?" rows="5">
+        </textarea>
+        <br />
+        Daily Goals!
+        <br />
+        <textarea placeholder="Daily Goals!" rows="5"></textarea>
+        <br />
+      </tr>
+    </thead>
+  </table>
+</template>
