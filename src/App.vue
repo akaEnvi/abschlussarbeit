f27eae5a87793251ae7958f2e126108302ced78e
Sascha Dietrich <sascha_dietrich@outlook.com>
2022-10-14 09:26:55 +0200

    debug

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <template>
   <nav>
     <router-link to="/">Index</router-link> |
-
     <router-link to="/HubView">Hub</router-link> |
     <router-link to="/CheckInView">CheckIn</router-link> |
     <router-link to="/CheckOutView">CheckOut</router-link> |
